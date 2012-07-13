e08048a90f279d3c7c19abcc777b5559c25a77a9
Coby Chapple <coby@cobychapple.com>
2012-07-13 23:01:19 +1000

    outline app download list

---
+++ bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Bootstrap - App Download List
+
+# Sublime
+curl http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.dmg
+
+# Dropbox
+# 1Password
+# Tuxera?
+# Skitch
+# Chrome, Firefox
+# iA Writer
+# Sparrow
+# Twitter?
+# Caffeine
+# Transmit/Coda?
+# Skype
+# Adobe Suite (+Lightroom?)
+# Alfred
+# AppZapper
+# Billings
+# MYOB?
+# CloudApp
+# Daisy Disk
+# GHfM
+# iTerm
+# MacVim
+# MAMP
+# VMware
+# Textmate?
+# Things?
+# Tor
+# Transmission
+# TrueCrypt
+# Viscosity
+# xScope
