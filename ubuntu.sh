49431c53ac7ea9099bc64af31bdea7ba712cdf50
Coby Chapple <coby@cobychapple.com>
2012-08-04 11:39:16 +1000

    start a ubuntu script

---
+++ ubuntu.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Ubuntu Laptop Setup Script
+# Much taken from thoughtbot’s laptop setup script before they removed Ubuntu:
+# https://github.com/thoughtbot/laptop/blob/bbe4b5c3e877afbadde71ab8ef67bd6017375fcd/ubuntu
+
+sudo add-apt-repository ppa:zedtux/naturalscrolling
+sudo apt-get update -y
+sudo apt-get install naturalscrolling -y
+sudo apt-get install xclip -y
+
+# Downloads
+# Gnome Shell
+# Google Chrome
+# Dropbox
+# Sublime Text
