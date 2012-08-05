f33b794b888112f04b01946bc507874666f2cffe
Coby Chapple <coby@cobychapple.com>
2012-08-06 08:22:03 +1000

    add to ubuntu script

--- ubuntu.sh
+++ ubuntu.sh
@@ -9,6 +9,35 @@ sudo apt-get update -y
 sudo apt-get install naturalscrolling -y
 sudo apt-get install xclip -y
 
+# install rbenv and various rubies
+# https://gist.github.com/1254518
+
+apt-get -y install build-essential
+apt-get -y install git-core
+
+# Install rbenv:
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+
+# Add rbenv to the path:
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .bash_profile
+echo 'eval "$(rbenv init -)"' >> .bash_profile
+source ~/.bash_profile
+
+# Install ruby-build:
+pushd /tmp
+  git clone git://github.com/sstephenson/ruby-build.git
+  cd ruby-build
+  ./install.sh
+popd
+
+# rbenv install <xyz>
+
+rbenv rehash
+
+# Install RubyGems
+
+
+
 # Downloads
 # Gnome Shell
 # Google Chrome
