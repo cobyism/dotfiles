0416502106a172f47d8cc5aa3d4c3b2545ef8c75
Coby Chapple <coby@cobychapple.com>
2013-05-05 14:04:24 +0100

    more /bin stuff, a speedtest.net dotjs script, alias change

---
+++ dotjs/js.symlink/speedtest.net.js
@@ -0,0 +1,11 @@
+$('#home-header-container').hide();
+$('#st-info').hide();
+$('#st-actions').hide();
+$('#ad-top-wrap').hide();
+$('.ad-vertical-container').hide();
+$('#home-features').hide();
+$('#footer').hide();
+$('#home-content-container').css('background', '#fff');
+$('#st-flash')
+  .css('width', '100%')
+  .css('text-align', 'center');
