88a1884d043b00d5a6aa420b53e518bf5186d94f
Coby Chapple <coby@cobychapple.com>
2013-10-07 10:51:57 +0100

    start hacking on my prompt a bit

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -33,15 +33,39 @@ function rbenv_version {
   echo `rbenv version | cut -f 1 -d " "`
 }
 
+function collapse_user {
+    echo $($USER | sed -e "s,^coby,,")
+}
+
+function collapse_hostname {
+    echo $(hostname -s | sed -e "s,^Vulcan,,")
+}
+
+# PROMPT=''
+# if [ $USER != "coby" ]; then
+#   PROMPT="$PROMPT%{$fg[magenta]%}$(collapse_user)%{$reset_color%}"
+# fi
+# if [ $(hostname -s) != "Vulcan" ]; then
+#   if [ $USER != "coby" ]; then
+#     PROMPT="$PROMPT at "
+#   fi
+#   PROMPT="$PROMPT%{$fg[yellow]%}$(collapse_hostname)%{$reset_color%} in "
+# fi
+# PROMPT="$PROMPT%{$fg_bold[green]%}$(collapse_pwd)%{$reset_color%}$(hg_prompt_info)$(git_prompt_info)
+# $(virtualenv_info)$(prompt_char) "
+
 PROMPT='
 %{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}$(collapse_pwd)%{$reset_color%}$(hg_prompt_info)$(git_prompt_info)
 $(virtualenv_info)$(prompt_char) '
 
-# RPROMPT='%{$fg[red]%}$(rbenv_version)%{$reset_color%}'
+
+RPROMPT='%{$fg[red]%}$(rbenv_version)%{$reset_color%}'
 RPROMPT=''
 
-ZSH_THEME_GIT_PROMPT_PREFIX=" on %{$fg[magenta]%}"
+ZSH_THEME_GIT_PROMPT_PREFIX=" ⌥ %{$fg[magenta]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%} ✘"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[red]%} ??"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%} ✔"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%} •"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[red]%} •"
+ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%} •"
+
+# source ~/.powerline/powerline/bindings/zsh/powerline.zsh
